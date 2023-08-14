ad45d392a6b3ea6377c133a164bd76765665aa07
Iaranagouda Adur <fegoudaa@gmail.com>
2023-08-15 00:10:31 +0530

    Added files related to frontend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import BasicInfo from './Components/AddProperty/Add-Basic-info/BasicInfo';
-import PropertyDetails from './Components/AddProperty/Add-property-details/PropertyDetails';
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import AddProperty from './pages/AddProperty'
+import HomePage from './pages/HomePage'
 
-function App() {
-  return <>
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<BasicInfo />} />
-      <Route path='/propertydetails' element={<PropertyDetails />} />
-    </Routes>
-  </BrowserRouter>
-  </>
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ViewPage from './pages/ViewPage';
+import Update from './pages/Update'
+import Protected from './Protected'
+const App = () => {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<SignIn/>}/>
+            <Route path='/signup' element={<SignUp/>} />
+            <Route path='/addproperty' element={<Protected><AddProperty/></Protected>} />
+            <Route path='/home' element={<Protected><HomePage/></Protected>}/>
+            <Route path='/viewpage' element={<Protected><ViewPage/></Protected>}/>
+            <Route path='/update' element={<Protected><Update/></Protected>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
-
-export default App;
+export default App
